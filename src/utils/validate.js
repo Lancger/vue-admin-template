441e6456c13db482c0b0889a3ecc71165fa5d531
Administrator <admin@example.com>
2019-07-31 06:59:19 +0800

    取消前端限制admin和editor账号登录的校验限制

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -15,6 +15,10 @@ export function isExternal(path) {
  * @returns {Boolean}
  */
 export function validUsername(str) {
+  return true
+  /**
+  str.trim()去掉字符串的空格
   const valid_map = ['admin', 'editor']
   return valid_map.indexOf(str.trim()) >= 0
+  */
 }
